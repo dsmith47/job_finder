f86657c9f4be4400a7c8391a56303baccd3508ed
dsmith47 <dougsmith2015@gmail.com>
2023-09-13 15:39:05 -0700

    script for inspecting how oune crawler crawls one page for debugging purposes

---
+++ debug_crawler.py
@@ -0,0 +1,23 @@
+import sys
+
+from jc_lib.companies.Google import GoogleCrawler
+from jc_lib.companies.Apple import AppleCrawler
+from jc_lib.companies.Microsoft import MicrosoftCrawler
+
+
+if __name__ == "__main__":
+  crawler_code = sys.argv[1]
+  url = sys.argv[2]
+
+  crawler = None
+  time = "time placeholder"
+  if crawler_code == "Google":
+    crawler = GoogleCrawler(time)
+  elif crawler_code == "Apple":
+    crawler = AppleCrawler(time)
+  elif crawler_code == "Microsoft":
+    crawler = MicrosoftCrawler(time)
+
+  report_items = [crawler.post_process(i) for i in crawler.extract_job_list_items(url)]
+  for i in report_items:
+    print(i + i.original_ad + "\n")
