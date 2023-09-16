358994956fcfd6130b64062e1d96dabbe19624ef
dsmith47 <dougsmith2015@gmail.com>
2023-09-15 21:27:59 -0700

    Fixing initial and post-processing on Apple Crawler

--- jc_lib/companies/Apple.py
+++ jc_lib/companies/Apple.py
@@ -1,4 +1,7 @@
+import re
+
 from jc_lib.crawlers import SoupCrawler
+
 
 class AppleCrawler(SoupCrawler):
   JOB_SITE_URLS = [ # NY Jobs
@@ -17,10 +20,12 @@ class AppleCrawler(SoupCrawler):
     link_items = bs_obj.find_all(href=True)
     for a in link_items:
       job_url = None
+      job_title = ''
       if a['href'].startswith('/en-us/details/'):
           job_url = self.url_root + a['href']
+          job_title = re.compile('See full role description').sub('', a.get_text())
       if not job_url: continue
-      output.append(self.make_report_item(job_url=job_url))
+      output.append(self.make_report_item(job_title=job_title, job_url=job_url))
     return output
 
   def post_process(self, report_item, driver):
@@ -28,8 +33,7 @@ class AppleCrawler(SoupCrawler):
     bs_obj = self.query_page(report_item.url)
     text_nodes = [i.get_text() for i in bs_obj.findAll(text=True)]
     i = 0
-    while len(text_nodes[i].strip()) < 1: i = i + 1
-    report_item.job_title = text_nodes[i]
+    while i < len(text_nodes) and not text_nodes[i].strip().find(report_item.job_title) >= 0: i += 1
     report_item.original_ad = ''.join(text_nodes[i+1:])
     return report_item
 
