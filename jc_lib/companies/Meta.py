d983ea554da827b7b62321e242b64277d89128d4
dsmith47 <dougsmith2015@gmail.com>
2023-09-16 19:22:40 -0700

    Give up on crawling when meta blocks the query

--- jc_lib/companies/Meta.py
+++ jc_lib/companies/Meta.py
@@ -58,7 +58,6 @@ class MetaCrawler(SeleniumCrawler):
         # close and return to the job list
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
-        #time.sleep(60000)
       except NoSuchElementException:
         break
       tabIndex += 1
