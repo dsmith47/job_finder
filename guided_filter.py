6d12adfc3dcb0bf627f62e5731dfc044935b1745
dsmith47 <dougsmith2015@gmail.com>
2023-09-17 23:57:55 -0700

    new filters for certain location/specializations that I can't apply to

--- guided_filter.py
+++ guided_filter.py
@@ -17,6 +17,7 @@ def test_job_title(report_item):
   if "Distinguished" in test_text: return test_text
   if "Artist" in test_text: return test_text
   if "Manager" in test_text: return test_text
+  if "Intern" in test_text: return test_text
   return False
 
 def test_6years_experience(report_item):
@@ -25,6 +26,32 @@ def test_6years_experience(report_item):
   years_asked = years_asked + re.findall('((\d)\+ years\' experience)', test_text, re.IGNORECASE)
   for y in years_asked:
     if int(y[1]) > 5: return y[0]
+  return False
+
+def test_wrong_specialization(report_item):
+  test_text = extract_ad_text(report_item)
+  output_text = "Underqualified field - reference to {}."
+  # Security works its way into a lot of descriptions apropos of nothing.
+  # It's really only a meaningful find in the job title.
+  if "security" in report_item.job_title: return output_text.format("Security")
+  if "Security" in report_item.job_title: return output_text.format("Security")
+  if "iOS" in test_text: return output_text.format("iOS")
+  if "Embedded Systems" in test_text: return output_text.format("Embedded Systems")
+  if "Embedded systems" in test_text: return output_text.format("Embedded Systems")
+  if "embedded systems" in test_text: return output_text.format("Embedded Systems")
+  if "Embedded Device" in test_text: return output_text.format("Embedded Device")
+  if "Embedded device" in test_text: return output_text.format("Embedded Device")
+  if "embedded device" in test_text: return output_text.format("Embedded Device")
+
+  return False
+
+def test_wrong_country(report_item):
+  test_text = extract_ad_text(report_item)
+  output_text = "Bad country - reference to {}."
+
+  if "Poland" in test_text: return output_text.format("Poland")
+  if "India" in test_text: return output_text.format("India")
+
   return False
 
 def ask_ignore_item(report_item, test_string):
@@ -39,15 +66,27 @@ def ask_ignore_item(report_item, test_string):
  report_item.is_ignored = True
 
 def inspect(report_item):
+  """
   if report_item.is_ignored: return
-
+  test_result = test_wrong_country(report_item)
+  if test_result:
+      ask_ignore_item(report_item, test_result)
+  """
+  if report_item.is_ignored: return
   test_result = test_job_title(report_item)
   if test_result:
       ask_ignore_item(report_item, "Title seems irrelevant: {}".format(test_result))
 
+  if report_item.is_ignored: return
   test_result = test_6years_experience(report_item)
   if test_result:
       ask_ignore_item(report_item, "Job seems to require >6y experience.: {}".format(test_result))
+  
+  if report_item.is_ignored: return
+  test_result = test_wrong_specialization(report_item)
+  if test_result:
+      ask_ignore_item(report_item, test_result)
+
 
 if __name__ == "__main__":
   infile_path = sys.argv[1]
@@ -63,6 +102,7 @@ if __name__ == "__main__":
   for row in file_reader:
     report_item = ReportItem.from_row(header, row)
     inspect(report_item)
+    print(report_item)
     output_writer.writerow(report_item.as_array())
 
   infile.close()
