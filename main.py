7d2660a72a07567e355676d39951b25c1bd22cf7
dsmith47 <dougsmith2015@gmail.com>
2023-09-27 11:19:04 -0700

    use-crawlers is now include-crawlers, better match with --exclude-crawlers

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
   parser.add_argument('--output-dir', type=str, default="output/")
   parser.add_argument('--num-crawlers', type=int, default=7, help="The number of workers to commit to web crawling (min 1)")
   parser.add_argument('--num-item-processors', type=int, default=1, help="The number of workers to commit to processing items produced in crawling (min 1)")
-  parser.add_argument('--use-crawlers', type=list_of_strings, default="")
+  parser.add_argument('--include-crawlers', type=list_of_strings, default="")
   parser.add_argument('--exclude-crawlers', type=list_of_strings, default="")
 
   args = parser.parse_args()
