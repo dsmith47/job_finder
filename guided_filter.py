21cf23349f09e4c2b3b6ec1b5f902458c25635d6
dsmith47 <dougsmith2015@gmail.com>
2023-09-30 12:41:06 -0700

    guided filter can now exit early without filtering all entries

--- guided_filter.py
+++ guided_filter.py
@@ -14,18 +14,19 @@ def extract_ad_text(report_item):
 # Prompt user about ignoring item, apply asked-for change,
 # report if change was made
 def ask_ignore_item(report_item, test_string):
- print(extract_ad_text(report_item))
- print(report_item)
- print(test_string.strip())
- command = None
- while command != "y" and command != "n":
-   command = input("Ignore Job (y/n)? ")
- return_code = False
- if command == "y":
-   report_item.is_ignored = True
-   return_code = True
- print("#################### INPUT BREAK ############################")
- return return_code
+  print(extract_ad_text(report_item))
+  print(report_item)
+  print(test_string.strip())
+  command = None
+  while command != "y" and command != "n" and command != "q":
+    command = input("Ignore Job (y - yes, n - no, q - quit)? ")
+  return_code = SIGNAL_CONTINUE
+  if command == "y":
+    report_item.is_ignored = True
+    return_code = SIGNAL_IGNORE
+  elif command == "q": return_code = SIGNAL_QUIT
+  print("#################### INPUT BREAK ############################")
+  return return_code
 
 # Checks the title for terms that imply low-utility
 def test_job_title(report_item, ignore_item_ui_fn):
@@ -51,27 +52,32 @@ def test_job_title(report_item, ignore_item_ui_fn):
                       "Research Scientist"]
   preservecase_items = ["IT",
                         "iOS"]
-  # 'in' statement outperforms regex for simple string finding
+  user_signal = SIGNAL_CONTINUE
   for term in ignorecase_items:
+    # 'in' statement outperforms regex for simple string finding
     if term.lower() in report_item.job_title.lower():
-      if ignore_item_ui_fn(report_item, prompt_text.format(term)):
-        return True
+      user_signal = ignore_item_ui_fn(report_item, prompt_text.format(term))
+      if user_signal == SIGNAL_IGNORE: return user_signal
+      elif user_signal == SIGNAL_QUIT: return user_signal
   for term in preservecase_items:
     if term in report_item.job_title:
-      if ignore_item_ui_fn(report_item, prompt_text.format(term)):
-        return True
-  return False
+      user_signal = ignore_item_ui_fn(report_item, prompt_text.format(term))
+      if user_signal == SIGNAL_IGNORE: return user_signal
+      elif user_signal == SIGNAL_QUIT: return user_signal
+  return user_signal
 
 
 def test_experience_gt6years(report_item, ignore_item_ui_fn):
   test_text = extract_ad_text(report_item)
   query_text = "Job seems to require >6y experience: {}"
   years_asked = re.findall(r'((\n|^)(\d+)\++ years.*(\n|$))', test_text, re.IGNORECASE)
+  user_signal = SIGNAL_CONTINUE
   for y in years_asked:
     if int(y[2]) > 5: 
-      if ignore_item_ui_fn(report_item, query_text.format(y[0])):
-        return True
-  return False
+      user_signal = ignore_item_ui_fn(report_item, query_text.format(y[0]))
+      if user_signal == SIGNAL_IGNORE: return user_signal
+      elif user_signal == SIGNAL_QUIT: return user_signal
+  return user_signal
 
 
 def test_body_unfamiliar_technology(report_item, ignore_item_ui_fn):
@@ -80,12 +86,14 @@ def test_body_unfamiliar_technology(report_item, ignore_item_ui_fn):
   ignorecase_items = ["windows server",
                       "copywrite",
                       "copywriting"]
-  # 'in' statement outperforms regex for simple string finding
+  user_signal = SIGNAL_CONTINUE
   for term in ignorecase_items:
+    # 'in' statement outperforms regex for simple string finding
     if term.lower() in report_item.job_title.lower():
-        if ignore_item_ui_fn(report_item, prompt_text.format(term)):
-          return True
-  return False
+      user_signal = ignore_item_ui_fn(report_item, prompt_text.format(term))
+      if user_signal == SIGNAL_IGNORE: return user_signal
+      elif user_signal == SIGNAL_QUIT: return user_signal
+  return user_signal 
 
 
 def test_body_post_removed(report_item, ignore_item_ui_fn):
@@ -93,12 +101,14 @@ def test_body_post_removed(report_item, ignore_item_ui_fn):
   query_text = "Job may no longer be present. Found text: '{}'"
   ignorecase_items = ["job not found",
                       "no longer accepting applications"]
-  # 'in' statement outperforms regex for simple string finding
+  user_signal = SIGNAL_CONTINUE
   for term in ignorecase_items:
+    # 'in' statement outperforms regex for simple string finding
     if term.lower() in report_item.job_title.lower():
-        if ignore_item_ui_fn(report_item, prompt_text.format(term)):
-          return True
-  return False
+      user_signal = ignore_item_ui_fn(report_item, prompt_text.format(term))
+      if user_signal == SIGNAL_IGNORE: return user_signal
+      elif user_signal == SIGNAL_QUIT: return user_signal
+  return SIGNAL_CONTINUE
 
 
 def test_body_wrong_country(report_item, ignore_item_ui_fn):
@@ -114,16 +124,19 @@ def test_body_wrong_country(report_item, ignore_item_ui_fn):
   # Note that we can't include any 2-letter code for a US state/territory/city
   preservecase_items = ["UK",
                         "PL"]
-  # 'in' statement outperforms regex for simple string finding
+  user_signal = SIGNAL_CONTINUE
   for term in ignorecase_items:
+    # 'in' statement outperforms regex for simple string finding
     if term.lower() in report_item.job_title.lower():
-      if ignore_item_ui_fn(report_item, prompt_text.format(term)):
-        return True
+      user_signal = ignore_item_ui_fn(report_item, prompt_text.format(term))
+      if user_signal == SIGNAL_IGNORE: return user_signal
+      elif user_signal == SIGNAL_QUIT: return user_signal
   for term in preservecase_items:
     if term in report_item.job_title:
-      if ignore_item_ui_fn(report_item, prompt_text.format(term)):
-        return True
-  return False
+      user_signal = ignore_item_ui_fn(report_item, prompt_text.format(term))
+      if user_signal == SIGNAL_IGNORE: return user_signal
+      elif user_signal == SIGNAL_QUIT: return user_signal
+  return user_signal
 
 
 def inspect(report_item):
@@ -139,6 +152,16 @@ def inspect(report_item):
   if test_body_wrong_country(report_item, ignore_item_fn): return
   if test_body_unfamiliar_technology(report_item, ignore_item_fn): return
 
+SIGNAL_CONTINUE = 1
+SIGNAL_IGNORE = 2
+SIGNAL_QUIT = 3
+
+def apply_test(test_fn, report_item):
+  # A function that accepts a report item and returns a Boolean
+  # representing whether an update happened. We expect it to prompt
+  # user
+  ignore_item_fn = ask_ignore_item
+  return test_fn(report_item, ignore_item_fn)
 
 if __name__ == "__main__":
   infile_path = sys.argv[1]
@@ -151,9 +174,30 @@ if __name__ == "__main__":
   output_writer = csv.writer(outfile)
   output_writer.writerow(ReportItem.header())
 
+  tests = [test_job_title,
+           test_body_post_removed,
+           test_experience_gt6years,
+           test_body_wrong_country,
+           test_body_unfamiliar_technology]
+
+  # Interactive read-test-write
   for row in file_reader:
     report_item = ReportItem.from_row(header, row)
     inspect(report_item)
+    user_signal = None
+    for test in tests:
+      if report_item.is_ignored: break
+      user_signal = apply_test(test, report_item)
+      if user_signal == SIGNAL_CONTINUE: continue
+      elif user_signal == SIGNAL_IGNORE:
+        report_item.is_ignored = True
+        continue
+      elif user_signal == SIGNAL_QUIT: break
+    output_writer.writerow(report_item.as_array())
+    if user_signal == SIGNAL_QUIT: break
+  # Read-write step (in case user quit interactive mode)
+  for row in file_reader:
+    report_item = ReportItem.from_row(header, row)
     output_writer.writerow(report_item.as_array())
 
   infile.close()
